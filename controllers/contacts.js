d7b256dc13fdadd4c294b96ea704e764748cbb0f
byui-titus <Titusrayz33@gmail.com>
2025-05-09 19:34:30 +0300

    tete

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -12,10 +12,11 @@ const mongodb = require('../data/database');
  }
 
  const getSingle = async (req, res) => {
+    const contactId = new ObjectId(req.params.id);
     const result = await mongodb.getDatabase().db().collection('contacts').find({_id: contactId});
     result.toArray().then((contacts) =>{
-        res.setHeader('Cotent-Type', 'applicatioon/json');
-        res.staus(200).json(contacts[0]);
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(contacts[0]);
     });
  }
 
