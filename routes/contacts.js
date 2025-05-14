118402f40a8ffdcac8212eb61b74102458ec820b
byui-titus <Titusrayz33@gmail.com>
2025-05-14 04:33:28 +0300

    swagger

--- routes/contacts.js
+++ routes/contacts.js
@@ -7,4 +7,10 @@ router.get('/', ContactCotroller.getAll);
 
 router.get('/:id', ContactCotroller.getSingle);
 
+router.post('/', ContactCotroller.createContact);
+
+router.put('/:id', ContactCotroller.updateContact);
+
+router.delete('/:id', ContactCotroller.deleteContact);
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {res.send('hello there');});
+router.use('/', require('./swagger'));
+
+router.get('/', (req, res) => {
+    
+    res.send('hello there');
+});
 
 router.use('/contacts', require ('./contacts'));
 
